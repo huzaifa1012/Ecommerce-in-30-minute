f2c9ab43635285d06dd49557438087a57845a7ac
huzaifahux <www.pakteach@gmail.com>
2023-03-26 16:18:20 +0500

    tailwind replaced by bootstrap

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ function buy() {
 
 
 let func = async () => {
+  
+
     let response = await axios.get('https://dummyjson.com/products/')
     let data = response.data.products
     console.log(data)
     data.map((data) => {
         return (
-            hero.innerHTML += `<div class="cardMain">
+            
+            
+            hero.innerHTML += ` <div class="cardMain">
             <div class="card">
                 <img src="${data.images[2]}">
                 <div class="card-down">
-                    <h3>${data.title.slice(0, 10)}</h3>
-                    <h3> $${data.price}</h3>
+                    <h6>${data.title.slice(0, 10)}</h6>
+                    <h6> $${data.price}</h6>
                 </div>
                 <button onclick="buy()">Buy Now</button>
             </div>
